bf1cefcb77ec08e221066c35a1a18f40ca1603e7
dylan_hsieh <dylan_hsieh@hiiir.com>
2016-09-14 16:56:58 +0800

    make it better

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -25,6 +27,38 @@ import (
 var mess = &Messenger{}
 
 func main() {
+	// mongo
+	const (
+		Host       = "ds011725.mlab.com:11725"
+		Username   = "dylan_hsieh"
+		Password   = "2juxuuux"
+		Database   = "message"
+		Collection = "messageResponse"
+	)
+	session, err := mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:    []string{Host},
+		Username: Username,
+		Password: Password,
+		Database: Database,
+		DialServer: func(addr *mgo.ServerAddr) (net.Conn, error) {
+			return tls.Dial("tcp", addr.String(), &tls.Config{})
+		},
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	coll := session.DB(Database).C(Collection)
+
+	// Find the number of games won by Dave
+	gamesWon, err := coll.Find(bson.M{"response": "超可愛"}).Count()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%d games.\n", gamesWon)
+	//
 	port := os.Getenv("PORT")
 	log.Println("Server start in port:", port)
 	mess.VerifyToken = os.Getenv("TOKEN")
