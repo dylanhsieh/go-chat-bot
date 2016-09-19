63ae68956d19d7bfcb1ab70bcdee1613b5a274ba
dylan_hsieh <dylan_hsieh@hiiir.com>
2016-09-19 11:24:03 +0800

    make it better

--- main.go
+++ main.go
@@ -56,7 +56,6 @@ func main() {
 		err := coll.Find(bson.M{}).All(&result)
 
 		if err == nil {
-			log.Println("mongo message", result)
 			fmt.Println("mongo message: ", result)
 		} else {
 			log.Println("read fail", err)
